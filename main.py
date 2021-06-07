624cfe7cc9137c8f99eafc6a6130a5d19d38d226
ShubhangiKukreti <55923146+ShubhangiKukreti@users.noreply.github.com>
2021-06-07 14:17:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+
+def random_colours():
+    r = random.randint(0, 255)
+    g = random.randint(0,255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+directions = [0, 90, 180, 270]
+tim.speed("fastest")
+tim.pensize(15)
+
+for _ in range(200):
+    colour = random_colours()
+    tim.color(colour)
+    tim.forward(100)
+    tim.setheading(random.choice(directions))
+
+
+
+screen.exitonclick()
